312de8728c628c6ba8309d57701eb43dce9b901f
MohamedKaka12 <129756595+MohamedKaka12@users.noreply.github.com>
2024-05-15 22:45:20 +0200

    image try again

--- assets/js/app.js
+++ assets/js/app.js
@@ -37,7 +37,7 @@ particlesJS('particles-js',
         "image": {
           "src": "img/github.svg",
           "width": 100,
-          "height": 70
+          "height": 100
         }
       },
       "opacity": {
